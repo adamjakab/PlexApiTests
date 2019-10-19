1568e02f7de451d470ed7fcc61b7669488d383e8
jackisback <adaja@itu.dk>
2019-10-20 01:03:50 +0200

    cleanup

--- lib/storage_cleanup_handler.py
+++ lib/storage_cleanup_handler.py
@@ -9,6 +9,7 @@ class StorageCleanupHandler:
     _ssh_username = "jackisback"
     _ssh_host = "192.168.1.84"
     _exclude_folders = ["000_Completed_Torrents"]
+    _movie_extensions = ["mkv", "avi", "mp4"]
     _folder_list = []
 
     def __init__(self, plex):
@@ -18,10 +19,19 @@ class StorageCleanupHandler:
 
     def do_it(self):
         for folder in self._folder_list:
-            print("FOLDER: {0}".format(folder))
+            movie_files = self._getMovieFiles(folder)
+            print("FOLDER({0}): {1}".format(folder, movie_files))
 
 
+    def _getMovieFiles(self, folder):
+        files = []
+        for f in glob.glob(folder + "/**", recursive=False):
+            if os.path.isfile(f):
+                fn, fe = os.path.splitext(f)
+                if fe in self._movie_extensions:
+                    files.append(f)
 
+        return files
 
     def _listFolders(self):
         for f in glob.glob(self._movie_container_directory + "/**", recursive=False):
